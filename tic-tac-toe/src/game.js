13d9bcb3cc3eaa7d511ef98157ecd6d78f67f334
Matthew Yasul <m.yasul@okrasolar.com>
2022-01-19 09:34:09 +0800

    Add elements

--- tic-tac-toe/src/game.js
+++ tic-tac-toe/src/game.js
@@ -3,13 +3,28 @@ const EMPTY_BOARD = [
     ['','',''],
     ['','',''],
 ]
+const DEFAULT_FIRST_PLAYER = 'X'
+const PLAYERS = ['X', 'O']
 
-export const startGame = () => {
+export const startGame = (firstPlayer = DEFAULT_FIRST_PLAYER) => {
+    let currentPlayer = !!PLAYERS.indexOf(firstPlayer)
+
     // Add event listeners to all boxers
     const boxes = document.querySelectorAll('.box')
     for (const box of boxes) {
-        box.addEventListener('click', () => {
-            console.log("CLICKED!")
+        box.addEventListener('click', (event) => {
+            makeMove(PLAYERS[Number(currentPlayer)], box)
+
+            currentPlayer = !currentPlayer
         })
     }
+}
+
+const makeMove = (player, box) => {
+    if (!PLAYERS.includes(player)) throw new Error(`Invalid player: ${player}`)
+
+    const paragraph = document.createElement('p')
+    paragraph.textContent = player
+
+    box.appendChild(paragraph)
 }
